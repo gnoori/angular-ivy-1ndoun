f6bd3c6895eb761309fd0a6ad1ceb819239744f9
gnoori <58310285+gnoori@users.noreply.github.com>
2023-01-18 16:25:43 -0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, OnInit } from '@angular/core';
+import { of, from, map, tap, take } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
+
+  ngOnInit() {
+    from(['Lucy', 'Safedak', 'Pashmak', '0'])
+      .pipe(
+        tap((item) => {
+          if (item === '0') {
+            throw new Error('Not a cat');
+          }
+          console.log(item);
+        }),
+        map((item) => `${item} Noori`),
+        map((item) => `Cat  ${item}`)
+      )
+      .subscribe({
+        next: (name) => console.log(`Full Name: ${name}`),
+        error: (err) => console.log(`Error ${err}`),
+        complete: () => console.log('Completed'),
+      });
+  }
 }
